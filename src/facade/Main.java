05c704dfdfe81ce9077e5044f117461989b39c6b
Omar <aldulaimiomar111@gmail.com>
2025-10-08 22:02:38 +0300

    Method DONE

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -5,17 +5,14 @@ public class Main {
         ApiFacade facade = new ApiFacade();
 
         try {
-            // Chuck Norris joke
             String joke = facade.getAttributeValueFromJson(
                     "https://api.chucknorris.io/jokes/random", "value");
             System.out.println("Random joke: " + joke);
 
-            // FX Rates API example (base currency)
             String baseCurrency = facade.getAttributeValueFromJson(
                     "https://api.fxratesapi.com/latest", "base");
             System.out.println("Base currency: " + baseCurrency);
 
-            // Trigger error: wrong attribute
             facade.getAttributeValueFromJson(
                     "https://api.chucknorris.io/jokes/random", "notAKey");
 
